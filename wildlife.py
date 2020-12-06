a8cf5fc9939b34768eb56b29b8e95e3876436142
Christoph Perick <christoph.perick@gmail.com>
2020-12-06 14:34:09 +0100

    calc and show delta image

--- wildlife.py
+++ wildlife.py
@@ -4,7 +4,7 @@
 import argparse
 import json
 import numpy as np
-import cv2 as cv
+import cv2
 
 # read config
 ap = argparse.ArgumentParser()
@@ -16,29 +16,49 @@ resolution = tuple(config["resolution"])
 frame_rate = config["fps"]
 
 # capture video
-cap = cv.VideoCapture(0)
+average_frame = None
+cap = cv2.VideoCapture(0)
 
 # set the width and height, and UNSUCCESSFULLY set the exposure time
-cap.set(cv.CAP_PROP_FRAME_WIDTH, resolution[0])
-cap.set(cv.CAP_PROP_FRAME_HEIGHT, resolution[1])
-cap.set(cv.CAP_PROP_FPS, frame_rate)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, resolution[0])
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, resolution[1])
+cap.set(cv2.CAP_PROP_FPS, frame_rate)
 
 if not cap.isOpened():
     print("Cannot open camera")
     exit()
 while True:
     # Capture frame-by-frame
-    ret, frame = cap.read()
-    # if frame is read correctly ret is True
+    ret, capture_frame = cap.read()
     if not ret:
         print("can't read frame")
         break
-    # create gray image of the frame
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+
+    # resize the frame, convert it to grayscale, and blur it
+    #frame = imutils.resize(frame, width=500)
+    gray = cv2.cvtColor(capture_frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (21, 21), 0)
+
+    #frame = frame.array
+    # if the average frame is None, initialize it
+    if average_frame is None:
+        print("[INFO] starting background model...")
+        average_frame = gray.copy().astype("float")
+        #frame.truncate(0)
+        continue
+
+    # accumulate the weighted average between the current frame and
+    # previous frames, then compute the difference between the current
+    # frame and running average
+    cv2.accumulateWeighted(gray, average_frame, 0.5)
+    frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(average_frame))
+
     # show the resulting frame
-    cv.imshow('frame', gray)
-    if cv.waitKey(1) == ord('q'):
+    cv2.imshow('frame', gray)
+    cv2.imshow('frameDelta', frameDelta)
+    if cv2.waitKey(1) == ord('q'):
         break
 # When everything done, release the capture
 cap.release()
-cv.destroyAllWindows()
+cv2.destroyAllWindows()
