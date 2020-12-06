592f83d3ac9f86c70c9301e57603c916665081a0
Christoph Perick <christoph.perick@gmail.com>
2020-12-06 19:04:40 +0100

    extract contours and draw bounding box

--- wildlife.py
+++ wildlife.py
@@ -3,6 +3,8 @@
 # construct the argument parser and parse the arguments
 import argparse
 import json
+
+import imutils
 import numpy as np
 import cv2
 
@@ -15,6 +17,7 @@ config = json.load(open(args["config"]))
 resolution = tuple(config["resolution"])
 frame_rate = config["fps"]
 delta_threshold = config["delta_thresh"]
+min_area = config["min_area"]
 
 # capture video
 average_frame = None
@@ -30,21 +33,21 @@ if not cap.isOpened():
     exit()
 while True:
     # Capture frame-by-frame
-    ret, capture_frame = cap.read()
+    ret, frame_capture = cap.read()
     if not ret:
         print("can't read frame")
         break
 
     # resize the frame, convert it to grayscale, and blur it
     # frame = imutils.resize(frame, width=500)
-    gray = cv2.cvtColor(capture_frame, cv2.COLOR_BGR2GRAY)
-    gray = cv2.GaussianBlur(gray, (21, 21), 0)
+    frame_gray = cv2.cvtColor(frame_capture, cv2.COLOR_BGR2GRAY)
+    frame_gray = cv2.GaussianBlur(frame_gray, (21, 21), 0)
 
     # frame = frame.array
     # if the average frame is None, initialize it
     if average_frame is None:
         print("[INFO] starting background model...")
-        average_frame = gray.copy().astype("float")
+        average_frame = frame_gray.copy().astype("float")
         # frame.truncate(0)
         continue
 
@@ -52,19 +55,34 @@ while True:
     # previous frames
     # accumulateWeighted(src, dst, alpha)
     # dst(x, y) = (1−alpha)⋅dst(x, y) + alpha⋅src(x, y)
-    cv2.accumulateWeighted(gray, average_frame, 0.5)
+    cv2.accumulateWeighted(frame_gray, average_frame, 0.5)
 
     # compute the difference between the current frame and running average
-    frame_delta = cv2.absdiff(gray, cv2.convertScaleAbs(average_frame))
+    frame_delta = cv2.absdiff(frame_gray, cv2.convertScaleAbs(average_frame))
 
     # create monochrome frame via threshold and dilate it
     frame_delta_thresh = cv2.threshold(frame_delta, delta_threshold, 255,
                                        cv2.THRESH_BINARY)[1]
-    frame_delta_thresh = cv2.dilate(frame_delta_thresh, None, iterations=2)
+    frame_delta_thresh = cv2.dilate(frame_delta_thresh, None, iterations=5)
+
+    # extract contours an draw them in the origin frame
+    contours = cv2.findContours(frame_delta_thresh.copy(), cv2.RETR_EXTERNAL,
+                                cv2.CHAIN_APPROX_SIMPLE)
+    contours = imutils.grab_contours(contours)
+    cv2.drawContours(frame_gray, contours, -1, (0, 255, 0), 1)
+
+    # loop over the contours
+    for c in contours:
+        # if the contour is too small, ignore it
+        if cv2.contourArea(c) < min_area:
+            continue
+        # create and draw bounding box around the contour
+        (x, y, w, h) = cv2.boundingRect(c)
+        cv2.rectangle(frame_capture, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
     # show the resulting frame
-    cv2.imshow('captured frame', capture_frame)
-    cv2.imshow('prepared frame', gray)
+    cv2.imshow('captured frame', frame_capture)
+    cv2.imshow('prepared frame', frame_gray)
     cv2.imshow('frame delta', frame_delta_thresh)
     if cv2.waitKey(1) == ord('q'):
         break
