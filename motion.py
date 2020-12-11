741b31826926a8833891a3f8ef8859748a092490
Christoph Perick <christoph.perick@gmail.com>
2020-12-11 18:53:52 +0100

    moved motion detection in own module

---
+++ motion.py
@@ -0,0 +1,63 @@
+import cv2
+import imutils
+
+
+class MotionDetection:
+    """Motion Detection"""
+
+    def __init__(self, config):
+        self.average_frame = None
+        self.config = config
+
+    def detect_motion(self, frame_capture):
+        rect_list = []
+
+        # resize the frame, convert it to grayscale, and blur it
+        # frame = imutils.resize(frame, width=500)
+        frame_gray = cv2.cvtColor(frame_capture, cv2.COLOR_BGR2GRAY)
+        frame_gray = cv2.GaussianBlur(frame_gray, (21, 21), 0)
+
+        # frame = frame.array
+        # if the average frame is None, initialize it
+        if self.average_frame is None:
+            print("[INFO] starting background model...")
+            self.average_frame = frame_gray.copy().astype("float")
+            # frame.truncate(0)
+            return rect_list
+
+        # accumulate the weighted average between the current frame and
+        # previous frames
+        # accumulateWeighted(src, dst, alpha)
+        # dst(x, y) = (1−alpha)⋅dst(x, y) + alpha⋅src(x, y)
+        cv2.accumulateWeighted(frame_gray, self.average_frame, 0.5)
+
+        # compute the difference between the current frame and running average
+        frame_delta = cv2.absdiff(frame_gray, cv2.convertScaleAbs(self.average_frame))
+
+        # create monochrome frame via threshold and dilate it
+        frame_delta_thresh = cv2.threshold(frame_delta, self.config.delta_threshold, 255,
+                                           cv2.THRESH_BINARY)[1]
+        frame_delta_thresh = cv2.dilate(frame_delta_thresh, None, iterations=5)
+
+        # extract contours an draw them in the origin frame
+        contours = cv2.findContours(frame_delta_thresh.copy(), cv2.RETR_EXTERNAL,
+                                    cv2.CHAIN_APPROX_SIMPLE)
+        contours = imutils.grab_contours(contours)
+        cv2.drawContours(frame_gray, contours, -1, (0, 255, 0), 1)
+
+        # loop over the contours
+        for c in contours:
+            # if the contour is too small, ignore it
+            if cv2.contourArea(c) < self.config.min_area:
+                continue
+            # create box around the contour and add it to return list
+            rect_list.append(cv2.boundingRect(c))
+
+        # combined_rectangles = rectangle.combine_rectangles(rect_list)
+        # for r in combined_rectangles:
+        #    cv2.rectangle(frame, (r[0], r[1]), (r[0] + r[2], r[1] + r[3]), (0, 255, 0), 2)
+
+        # show the resulting frame
+        cv2.imshow('prepared frame', frame_gray)
+        cv2.imshow('frame delta', frame_delta_thresh)
+        return rect_list
--- wildlife.py
+++ wildlife.py
@@ -1,93 +1,19 @@
 #!/usr/bin/env python
 
-# construct the argument parser and parse the arguments
 import argparse
 import datetime
 import os
 import shutil
-
-import imutils
 import cv2
 
 from config import WildlifeConfig
-
-
-class MotionDetection:
-    """Motion Detection"""
-
-    def __init__(self):
-        self.average_frame = None
-
-    def detect_motion(self, frame_capture):
-        rect_list = []
-
-        # resize the frame, convert it to grayscale, and blur it
-        # frame = imutils.resize(frame, width=500)
-        frame_gray = cv2.cvtColor(frame_capture, cv2.COLOR_BGR2GRAY)
-        frame_gray = cv2.GaussianBlur(frame_gray, (21, 21), 0)
-
-        # frame = frame.array
-        # if the average frame is None, initialize it
-        if self.average_frame is None:
-            print("[INFO] starting background model...")
-            self.average_frame = frame_gray.copy().astype("float")
-            # frame.truncate(0)
-            return rect_list
-
-        # accumulate the weighted average between the current frame and
-        # previous frames
-        # accumulateWeighted(src, dst, alpha)
-        # dst(x, y) = (1−alpha)⋅dst(x, y) + alpha⋅src(x, y)
-        cv2.accumulateWeighted(frame_gray, self.average_frame, 0.5)
-
-        # compute the difference between the current frame and running average
-        frame_delta = cv2.absdiff(frame_gray, cv2.convertScaleAbs(self.average_frame))
-
-        # create monochrome frame via threshold and dilate it
-        frame_delta_thresh = cv2.threshold(frame_delta, config.delta_threshold, 255,
-                                           cv2.THRESH_BINARY)[1]
-        frame_delta_thresh = cv2.dilate(frame_delta_thresh, None, iterations=5)
-
-        # extract contours an draw them in the origin frame
-        contours = cv2.findContours(frame_delta_thresh.copy(), cv2.RETR_EXTERNAL,
-                                    cv2.CHAIN_APPROX_SIMPLE)
-        contours = imutils.grab_contours(contours)
-        cv2.drawContours(frame_gray, contours, -1, (0, 255, 0), 1)
-
-        # loop over the contours
-        for c in contours:
-            # if the contour is too small, ignore it
-            if cv2.contourArea(c) < config.min_area:
-                continue
-            # create box around the contour and add it to return list
-            rect_list.append(cv2.boundingRect(c))
-
-        # combined_rectangles = rectangle.combine_rectangles(rect_list)
-        # for r in combined_rectangles:
-        #    cv2.rectangle(frame, (r[0], r[1]), (r[0] + r[2], r[1] + r[3]), (0, 255, 0), 2)
-
-        # show the resulting frame
-        cv2.imshow('prepared frame', frame_gray)
-        cv2.imshow('frame delta', frame_delta_thresh)
-        return rect_list
+from motion import MotionDetection
 
 
 def create_video_filename(start_time, path):
     return "{}/{}-wildlife.avi".format(path, start_time.strftime("%Y%m%d-%H%M%S"))
 
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-c", "--config", required=True,
-                help="path to the JSON configuration file")
-args = vars(ap.parse_args())
-
-config = WildlifeConfig(args["config"])
-
-md = MotionDetection()
-
-# capture video
-average_frame = None
-is_recording = False
 last_activity = None
 recording_status = "OFF"
 recording_color = (255, 255, 225)
@@ -96,6 +22,15 @@ recording_filename = ""
 start_recording_time = None
 stop_recording_time = None
 video_out = None
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-c", "--config", required=True,
+                help="path to the JSON configuration file")
+args = vars(ap.parse_args())
+
+config = WildlifeConfig(args["config"])
+
+md = MotionDetection(config)
 
 # prepare video storage folder
 if config.clean_store_on_startup:
